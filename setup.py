4fcf7f25c18377cf5ee929d3ee86baa1455a0ad4
Dominik Dlugajczyk <d.dlugajczyk@clearcode.cc>
2017-09-01 14:45:24 +0200

    New version of morfdict.

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ install_requires = [
     'pyramid>=1.9',
     'PyYAML>=3.12',
     'colander>=1.3.3',
-    'morfdict>=0.4.5',
+    'morfdict>=0.4.6',
 ]
 
 if __name__ == '__main__':
     setup(
         name='qapla',
-        version='0.1',
+        version='0.2',
         packages=find_packages(),
         install_requires=install_requires,
         license='Apache License 2.0',
