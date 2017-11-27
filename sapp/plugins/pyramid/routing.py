16e670f4a814f4b253d82390f98f0d15ce738b8d
Dominik Dlugajczyk <d.dlugajczyk@clearcode.cc>
2017-11-27 19:36:56 +0100

    Move plugins to pyramid.

--- sapp/plugins/pyramid/routing.py
+++ sapp/plugins/pyramid/routing.py
@@ -1,5 +1,7 @@
 from yaml import load
 
+from sapp.plugin import Plugin
+
 
 class Routing(object):
 
@@ -96,3 +98,19 @@ class RouteYamlParser(object):
     def _convert_route_dict(self, name, element):
         element['controller'] = name + '.' + element['controller']
         return element
+
+
+class RoutingPlugin(Plugin):
+    """
+    Add routing to the pyramid app.
+    """
+
+    def __init__(self, routing_cls):
+        self.routing_cls = routing_cls
+
+    def start_plugin(self, configurator):
+        self.configurator = configurator
+
+    def start_web_plugin(self, pyramid):
+        self.routing = self.routing_cls(pyramid, self.configurator.paths)
+        self.routing.make()
