3ec3b5fb95ccfe42a718a2f892859f78795c8218
Dominik Dlugajczyk <d.dlugajczyk@clearcode.cc>
2020-09-14 18:48:04 +0200

    Remove webtest relative code.

--- sapp/plugins/pyramid/testing.py
+++ sapp/plugins/pyramid/testing.py
@@ -1,7 +1,6 @@
 from unittest.mock import MagicMock
 
 from pytest import fixture
-from webtest import TestApp
 
 from sapp.plugins.sqlalchemy.testing import BaseIntegrationFixture
 
@@ -27,17 +26,3 @@ class ViewFixtureMixin(object):
         obj = MagicMock()
         obj.registry = dict(application=app)
         return obj
-
-
-class BaseWebTestFixture(BaseIntegrationFixture):
-    UWSGI_KEY = 'wsgi'
-
-    @fixture(scope='module')
-    def wsgi_app(self, config):
-        if self.UWSGI_KEY not in self.SESSION_CACHE:
-            self.SESSION_CACHE[self.UWSGI_KEY] = config.make_wsgi_object()
-        return self.SESSION_CACHE[self.UWSGI_KEY]
-
-    @fixture
-    def fake_app(self, wsgi_app):
-        return TestApp(wsgi_app)
