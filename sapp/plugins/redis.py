777db7d20055793f4c58b0bc7e0b89be51871afd
Dominik Dlugajczyk <d.dlugajczyk@clearcode.cc>
2020-09-16 11:02:29 +0200

    Fix redis plugin. Use Redis instead of StrictRedis.

--- sapp/plugins/redis.py
+++ sapp/plugins/redis.py
@@ -1,21 +1,21 @@
-from redis import StrictRedis
+from redis import Redis
 
 from sapp.plugin import Plugin
 
 
 class RedisPlugin(Plugin):
-    SETTINGS_HOST_KEY = "redis:host"
-    SETTINGS_PORT_KEY = "redis:port"
-    SETTINGS_DB_KEY = "redis:db"
+    SETTINGS_HOST_KEY = "host"
+    SETTINGS_PORT_KEY = "port"
+    SETTINGS_DB_KEY = "db"
 
-    def start(self, configurator, ctx_key="redis"):
-        self.settings = configurator.settings
+    def __init__(self, ctx_key="redis"):
         self.ctx_key = ctx_key
 
     def enter(self, context):
+        settings = context.settings[self.ctx_key]
         params = dict(
-            host=self.settings[self.SETTINGS_HOST_KEY],
-            port=self.settings[self.SETTINGS_PORT_KEY],
-            db=self.settings[self.SETTINGS_DB_KEY],
+            host=settings[self.SETTINGS_HOST_KEY],
+            port=settings[self.SETTINGS_PORT_KEY],
+            db=settings[self.SETTINGS_DB_KEY],
         )
-        setattr(context, self.ctx_key, StrictRedis(**params))
+        setattr(context, self.ctx_key, Redis(**params))
